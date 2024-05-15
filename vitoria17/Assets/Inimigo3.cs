248975f789a22c658a19cb633a5810420d07af7d
Ferreiramaria <136329026+Ferreiramaria@users.noreply.github.com>
2024-05-15 16:02:41 -0300

    Bob Vitória
    Jogo Vitória

---
+++ vitoria17/Assets/Inimigo3.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inimigo3 : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    public float xSpeed;
+    private float fixSpeed;
+    private GameManager gM;
+    private float pInicialX;
+    public float limiteX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        gM = GameObject.Find("GameManager").GetComponent<GameManager>();
+        pInicialX = transform.position.x;
+        fixSpeed = xSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gM.isGameOver == false && gM.pause == false)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 1 * (-speed));
+        }
+        else if (gM.isGameOver == true || gM.pause == true)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
+
+        if (transform.position.y <= -6)
+        {
+            gM.pontuacao += 1;
+            Destroy(gameObject);
+        }
+
+        rb.velocity = new Vector2(fixSpeed, rb.velocity.y);
+        DeslocarHorizontal();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Progetil")
+        {
+            gM.pontuacao += 1;
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    private void DeslocarHorizontal()
+    {
+        if (transform.position.x >= pInicialX + limiteX)
+        {
+            fixSpeed = -xSpeed;
+        }
+        else if (transform.position.x <= pInicialX - limiteX)
+        {
+            fixSpeed = xSpeed;
+        }
+
+        if (gM.isGameOver == true || gM.pause == true)
+        {
+            fixSpeed = 0;
+        }
+    }
+}
